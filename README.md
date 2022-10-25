808503a6b6425c6831b7fa17c3e9e7a8f4582f71
Rad <killadog@gmail.com>
2022-10-25 11:55:30 +0300

    upd

--- README.md
+++ README.md
@@ -1,1 +1,82 @@
 # Robocoper
+*Simple backup tool based on [Robocopy](https://en.wikipedia.org/wiki/Robocopy)*
+
+![Powershell](https://img.shields.io/badge/Powershell-blue.svg)
+
+<p align="center">
+<img src="img/robocoper10.png" alt="Robocoper" width="520" />
+</p>
+
+### Features
+
+- Backup a batch of sources by one tool
+- Delete backups older than *N* days
+- Send email about work
+- Save compressed log 
+
+### Tested on
+
+- [Windows Server 2019](https://en.wikipedia.org/wiki/Windows_Server_2019)
+- [Powershell](https://docs.microsoft.com/ru-ru/powershell/scripting/install/installing-powershell) 7.2.7
+
+### Requirements
+
+- [Robocopy](https://en.wikipedia.org/wiki/Robocopy)
+
+### Preparation
+
+- Change lines in `email.json` for e-mail settings:
+```
+$EmailTo = 'example@example.com'
+$EmailFrom = 'example@example.com'
+$EmailPassword = '$eCr3tP@$sW0Rd'
+$EmailSmtpServer = 'smtp.example.com'
+$EmailSmtpPort = 587
+```
+- Uncomment this line to use TLS not SSL for e-mail
+
+`[System.Net.ServicePointManager]::SecurityProtocol = "Tls, TLS11, TLS12"`
+
+- To turn ON e-mail notifications
+
+`$SendEmail = $true`
+
+- To turn ON log saving
+
+`$SaveLog = $true`
+
+`$LogDir = "D:\Backup\logs"`
+
+### Usage
+
+In array `$Backups` insert line(s) for backup
+
+, @("`Name`", "`Source`", "`Destination`", "`DTL`", "`Parameters`")
+
+|Option|Explanation|Example|Default value|
+|---|---|---|:---:|
+|**Name**|Backup name|My code||
+|**Source**|Backup from|C:\code||
+|**Destination**|Backup to|D:\Backup||
+|**DTL**|Days to live<sup>[1]</sup>|10|0|
+|**Parameters**|Robocopy syntax|[Syntax](https://learn.microsoft.com/en-us/windows-server/administration/windows-commands/robocopy)|$DefaultParameters|
+
+*Note:* disable backups by `#` at the begin of line or `<# ... #>` for multiline.
+
+[1] - '`Days to live`' means that new backup create folder `NAME_YYYYMMDD_HHMMSS` in `Destination`. After it all folders and files older than `N` days will be deleted in `Destination`. If `DTL` is `""` backup will
+be in `Destination` with `default` or special parameters.
+<!--> Important
+To change codepage/encoding play with lines:
+```
+#chcp 866 | out-null
+#chcp 65001 | out-null
+...
+$body = Get-Content -Path $LogTmpFile | Out-String 
+```
+-->
+
+
+
+
+
+
---
+++ robocoper.ps1
@@ -0,0 +1,124 @@
+$DefaultParameters = "/MT:8 /E /NDL /NP /R:1 /W:1" # Default robocopy parameters
+$SendEmail = $true
+$SaveLog = $true
+if ($SaveLog) {
+    $LogDir = "D:\Backup\logs"
+}
+$str = "$($PSStyle.Foreground.BrightYellow)*$($PSStyle.Reset)" * 80 # Separation line for better log view
+
+$Backups = @(
+    , @("First Backup", "C:\Images", "D:\Backup\First", "0", "")
+    , @("Second Backup", "C:\Documents", "D:\Backup\Second", "4", "/MT:8 /E /NDL /NP /R:1 /W:1")
+    # , @("Third Backup", "C:\Bases", "D:\Backup\Third", "4", "")
+    , @("Fourth Backup", "C:\Music", "D:\Backup\Fourth", "0", "")
+    <# , @("Fifth Backup", "C:\PDFs", "D:\Backup\Fifth", "10", "/MT:8 /E /NDL /NP /R:1 /W:1")
+    , @("Sixth Backup", "C:\XXX", "D:\Backup\Sixth", "4", "") #>
+)
+
+$LogTmpFile = (New-TemporaryFile).FullName | Rename-Item -NewName { $_ -replace 'tmp$', 'txt' } –PassThru
+
+Start-Transcript -Append $LogTmpFile -UseMinimalHeader 
+
+$TotalTime = Measure-Command {
+    $BackupTable = @()
+    $Backups | ForEach-Object {
+        $RobocopyName = $_[0]
+        $RobocopySource = $_[1]
+        $RobocopyDestination = $_[2]
+        $RobocopyDaysToLive = $_[3]
+        $RobocopyParameters = $_[4]
+        Write-Host "$str`n[$($Backups.IndexOf($_) + 1)/$($Backups.Count)] Backup: '$RobocopyName'"
+        $StartTime = Get-Date -Format 'yyyy\/MM\/dd HH\:mm\:ss'
+        $BackupTime = Measure-Command { 
+            if ($RobocopyParameters -eq "") {
+                $RobocopyParameters = $DefaultParameters
+            }
+
+            if ($RobocopyDaysToLive -ne 0) {
+                $BackupFolder = $RobocopyDestination
+                $RobocopyDestination = "$RobocopyDestination\$($RobocopyName.Replace(" ","_"))_$(Get-Date -Format 'ddMMyyyy_HHmmss')" 
+            }
+
+            Write-Host "robocopy $RobocopySource $RobocopyDestination $RobocopyParameters"
+            robocopy $RobocopySource $RobocopyDestination $RobocopyParameters.split(' ') | Write-Host           
+        }
+ 
+        if ($LASTEXITCODE -le 3) {
+            $Result = "Success"
+            if ($RobocopyDaysToLive -ne 0) {
+                Write-Host "`nDelete files older than $RobocopyDaysToLive days:"
+                $DeleteFromDate = (Get-Date).AddMinutes(-$RobocopyDaysToLive)
+                $WhatToDelete = Get-ChildItem -Path $BackupFolder | Where-Object { ($_.CreationTime -le $DeleteFromDate) -and ($_.LastWriteTime -le $DeleteFromDate) }
+                if ($WhatToDelete.Count -ne 0) {
+                    $WhatToDelete | Remove-Item -Recurse -Force -Verbose 
+                }
+                else {
+                    Write-Host "Nothing to delete"
+                }
+            }
+        }
+        else {
+            $Result = "Failed"
+        }
+
+        $number = $Backups.IndexOf($_) + 1
+        $BackupTable += [PSCustomObject] @{
+            'Number'      = "$(([string]$number).PadLeft(2,' '))/$($Backups.Count)"
+            'Name'        = $RobocopyName
+            'Result'      = "$Result ($LASTEXITCODE)"
+            'Backup time' = $BackupTime.ToString("hh\:mm\:ss")
+            'DaysToLive'  = $RobocopyDaysToLive
+            'Source'      = $RobocopySource
+            'Destination' = $RobocopyDestination
+            'Start'       = $StartTime
+            'End'         = Get-Date -Format 'yyyy\/MM\/dd HH\:mm\:ss'
+        }
+    }
+}
+
+if ($SaveLog) {
+    Write-Host "$str`nCompress Log file '$LogTmpFile' to '$LogDir\log_$(Get-Date -UFormat "%Y%m%d-%H%M%S").zip'"
+    Compress-Archive -Path $LogTmpFile -DestinationPath $LogDir\log_$(Get-Date -UFormat "%Y%m%d-%H%M%S").zip
+}
+
+Stop-Transcript
+
+$BackupTable = $BackupTable |  Format-Table -Property @{n = "Number"; e = { $_.Number }; a = "center" },
+@{n = "Name"; e = { $_.'Name' }; a = "center" },
+@{n = "Result"; e = { $_.'Result' }; a = "center" },
+@{n = "Backup time"; e = { $_.'Backup time' }; a = "center" },
+@{n = "DTL"; e = { $_.DaysToLive }; a = "center" },
+@{n = "Source"; e = { $_.'Source' }; a = "left" },
+@{n = "Destination"; e = { $_.'Destination' }; a = "left" }, 
+@{n = "Start time"; e = { $_.'Start' }; a = "center" },
+@{n = "End time"; e = { $_.'End' }; a = "center" } -AutoSize | Out-String -Width 512
+
+if ($SendEmail) {
+    $body = Get-Content -Path $LogTmpFile | Out-String 
+    $body = "Total backup time: $($TotalTime.ToString('hh\:mm\:ss'))`n$($BackupTable)" + $body
+    $email = get-content -Path $PSScriptRoot\email.json -Raw | ConvertFrom-Json
+    $EmailSubject = "robocopy"
+    $secpasswd = ConvertTo-SecureString $email.EmailPassword -AsPlainText -Force
+    $mycreds = New-Object System.Management.Automation.PSCredential ($email.EmailFrom, $secpasswd)
+    $encoding = [System.Text.Encoding]::UTF8
+    [System.Net.ServicePointManager]::SecurityProtocol = "Tls, TLS11, TLS12" # Uncomment this line for TLS not SSL
+    Send-MailMessage `
+        -To $email.EmailTo `
+        -Subject $EmailSubject `
+        -Body $body `
+        -SmtpServer $email.EmailSmtpServer `
+        -Credential $mycreds `
+        -Port $email.EmailSmtpPort `
+        -UseSsl `
+        -from "$($env:COMPUTERNAME) <$($email.EmailFrom)>" `
+        -Encoding $encoding `
+        -WarningAction:SilentlyContinue 
+    Write-Host "Send e-mail to $($email.EmailTo)"
+}
+
+$str
+Remove-Item -Path $LogTmpFile -Force -Verbose
+
+$str
+$BackupTable
+Write-Host "Total backup time: $($TotalTime.ToString('hh\:mm\:ss'))"
